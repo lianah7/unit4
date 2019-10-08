4c735d4f89816f13d4830115ef01b24e60f57b6f
liana <hilllia@ssfs.org>
2019-10-08 14:14:42 -0400

    did triangle exercise and worked on rock, paper, scissors exercise

--- daily_exercises.py
+++ daily_exercises.py
@@ -2,6 +2,8 @@
 # last updated October 3, 2019
 # daily exercises
 
+import random
+
 # exercise one
 number = int(input("Pick a number"))
 if (number % 2) == 1:
@@ -17,6 +19,35 @@ def is_divisible(num1, num2):
         return False
 
 
+# problem three
+def is_triangle(side1, side2, side3):
+    if (side1 >= side2 + side3) or (side3 >= side1 + side2) or (side2 >= side3 + side1):
+        print("No")
+    else:
+        print("Yes")
+
+
+def triangle_input():
+    side1 = int(input("Please enter a length for a side"))
+    side2 = int(input("Enter another length for the second side"))
+    side3 = int(input("Enter another length for the third side"))
+    is_triangle(side1, side2, side3)
+
+
+# rock paper scissors
+def computer_input():
+    print("Let's play rock, paper, scissors!")
+    print("Pick rock, paper, or scissors")
+    random.randint(1, 3)
+    rock = 1
+    paper = 2
+    scissors = 3
+
+
+def rock_paper_scissors(rock, paper, scissors):
+    computer_input()
+
+
 def main():
     num = int(input("Pick a number"))
     check = int(input("Pick another number"))
@@ -24,6 +55,7 @@ def main():
         print("True")
     else:
         print("False")
-
+    triangle_input()
+    rock_paper_scissors()
 
 main()
