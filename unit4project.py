d2c4b9c8257ec771f88c61380450745481ca32c6
liana <hilllia@ssfs.org>
2019-10-17 10:59:27 -0400

    finished unit four project

--- unit4project.py
+++ unit4project.py
@@ -24,13 +24,15 @@ def main():
     card2 = get_card()
     total = card1 + card2
     starting_cards(card1, card2, total)
-    input("Do you want another card? Enter 'y' for yes and 'n' for no.")
-    if 'y':
+    additional_cards = input("Do you want another card? Enter 'y' for yes and 'n' for no.")
+    card3 = get_card()
+    user_total = card1 + card2 + card3
+    if additional_cards =='y':
         card3 = get_card()
-        total = card1 + card2 + card3
-        print("Your new total is", total)
+        user_total = card1 + card2 + card3
+        print("Your new total is", user_total)
     else:
-        print("Your total is", total)
+        print("Your total is still", total)
     if total > 21:
         print("You lost!")
     else:
@@ -39,8 +41,10 @@ def main():
         dealer_total = card4 + card5
         print("The dealer's cards are", card4, "and", card5)
         print("The dealer's total is", dealer_total)
-        if card4 + card5 > total:
+        if dealer_total < user_total:
             print("You won!")
+        elif dealer_total == user_total:
+            print("You tied. The dealer wins!")
         else:
             print("You lost!")
 
